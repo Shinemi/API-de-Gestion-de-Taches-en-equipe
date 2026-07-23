4702198c220b99d26c3b886facb773346aa7004e
Shinemi <simon.lucas.83136@gmail.com>
2026-07-23 13:57:23 +0200

    Implement project creation functionality with validation

--- controllers/projectController.js
+++ controllers/projectController.js
@@ -3,9 +3,14 @@ const Project = require('../models/projectModel')
 
 const createProject = async (req , res) => {
     try {
-        
+        const project = new Project({
+            name : req.body.name,
+            description : req.body.description,
+        })
+        const newProduct = await project.save()
+        res.status(201).json(newProduct)
     } catch (error) {
-        
+        res.status(400).json({message: "erreur lors de la creation du projet",  error: error.message})
     }
 }
 
---
+++ models/projectModel.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose')
+
+const projectSchema = new mongoose.Schema({
+    name:{
+        type:String,
+        required:true
+    },
+    description:{
+        type: String,
+        required:true
+    },
+
+  
+})
+
+module.exports = mongoose.model('Projects',projectSchema)
